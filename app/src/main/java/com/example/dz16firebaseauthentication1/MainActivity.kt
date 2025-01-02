95ab85161f57a106134a118ecce445596d43d83d
OlegEfremov54 <ole_f@mail.ru>
2025-01-03 01:36:31 +0300

    Initial commit

---
+++ app/src/main/java/com/example/dz16firebaseauthentication1/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.dz16firebaseauthentication1
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.dz16firebaseauthentication1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var toolbarMain:Toolbar
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Тулбар
+        toolbarMain = findViewById(R.id.toolbarMain)
+        setSupportActionBar(toolbarMain)
+        title = " Электронная почта"
+        toolbarMain.subtitle = " Версия 1.Главная страница"
+        toolbarMain.setLogo(R.drawable.diplomamini)
+        //supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        //toolbarMain.setNavigationOnClickListener { onBackPressed() }
+
+
+    }
+
+    //Инициализация Меню
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.infoMenuMain -> {
+                Toast.makeText(
+                    applicationContext, "Автор Ефремов О.В. Создан 31.12.2024",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+
+            R.id.exitMenuMain -> {
+                Toast.makeText(
+                    applicationContext, "Работа приложения завершена",
+                    Toast.LENGTH_LONG
+                ).show()
+                finishAffinity()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+}
